47770e3b83e81a46b04e461985a0bad99be266a6
paulapivat <paul.apivat@gmail.com>
2020-09-03 16:40:13 +0700

    add several python setup

---
+++ courses/python/pycharm_setup.py
@@ -0,0 +1,6 @@
+import pandas
+flights = pandas.read_csv("flights.csv")
+flights = flights[flights['dest'] == 'ORD']
+flights = flights[['carrier', 'dep_delay', 'arr_delay']]
+flights = flights.dropna()
+print(flights.head())
